1e8c2ee2eb583b625be3274147dd1074b5adf419
Doyomir <doyomir@gmail.com>
2019-11-08 21:13:47 +0200

    initial commit

---
+++ src/main/java/com/advanced/academy/adventure/books/system/controller/HeroController.java
@@ -0,0 +1,33 @@
+package com.advanced.academy.adventure.books.system.controller;
+
+import com.advanced.academy.adventure.books.system.models.Hero;
+import com.advanced.academy.adventure.books.system.service.HeroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/hero")
+
+public class HeroController {
+
+    private final HeroService heroservice;
+
+    @Autowired
+    public HeroController(HeroService heroservice) {
+        this.heroservice = heroservice;
+    }
+
+
+    @GetMapping(value = "cusrtomer/{customerId}")
+    public List<Hero> getHeroList(@PathVariable("customerId") Integer customerId){
+        return heroservice.getHeroByCustomerId(customerId);
+
+    }
+
+    @GetMapping(value = "/{heroId}")
+    public Hero getHero(@PathVariable("heroId") Integer heroId){
+        return heroservice.getHeroById(heroId);
+    }
+}
